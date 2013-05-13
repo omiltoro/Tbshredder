02d4f326188981084b74a6e3d5ece680a4da48e0
Patrick <pattorolimo@gmail.com>
2013-05-13 14:54:55 +0300

    TB labtest form
    version 1(Android)

---
+++ gen/com/ampath/tbshredder/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ampath.tbshredder;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/ampath/tbshredder/MainActivity.java
@@ -0,0 +1,28 @@
+package com.ampath.tbshredder;
+import org.apache.cordova.DroidGap;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+
+public class MainActivity extends DroidGap {
+
+	//@Override
+	//protected void onCreate(Bundle savedInstanceState) {
+		//super.onCreate(savedInstanceState);
+		//setContentView(R.layout.activity_main);
+	//}
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
